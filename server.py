2b79307a28a674ae73beabdcacae8971fdd15640
calin <kalinpoiata@gmail.com>
2024-12-21 21:05:14 +0200

    Add json for teachers

--- server.py
+++ server.py
@@ -1,21 +1,20 @@
+import json
+
 from flask import Flask, render_template, request
 import random
 import numpy as np
-
 app = Flask(__name__)
 
 questions = {
-    1: 'Is your character yellow?',
-    2: 'Is your character bald?',
-    3: 'Is your character a man?',
-    4: 'Is your character short?',
+    1: 'Is the teacher male?',
+    2: 'Do they teach something science-related?',
+    3: 'Is the teacher bald (balding)?',
+    4: 'Is the teacher professional?',
 }
 
-characters = [
-    {'name': 'Homer Simpson',         'answers': {1: 1, 2: 1, 3: 1, 4: 0}},
-    {'name': 'SpongeBob SquarePants', 'answers': {1: 1, 2: 1, 3: 1, 4: 0.75}},
-    {'name': 'Sandy Cheeks',          'answers': {1: 0, 2: 0, 3: 0}},
-]
+f = open('./teachers.json', 'r')
+characters = json.load(f)
+f.close()
 
 questions_so_far = []
 answers_so_far = []
